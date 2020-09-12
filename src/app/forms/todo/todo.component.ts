233c3ffa781c1edcda6a87ac27ceade7ad891aca
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-12 14:13:26 +0530

    form working

--- src/app/forms/todo/todo.component.ts
+++ src/app/forms/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -9,7 +9,24 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TodoComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      date: [''],
+      comments: ['Default Comment'],
+    });
+  }
+
+  get date(): FormControl {
+    return this.form.controls['date'] as FormControl;
+  }
+
+  get comments(): FormControl {
+    return this.form.controls['comments'] as FormControl;
+  }
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('Form submit', this.form.value);
+  }
 }
