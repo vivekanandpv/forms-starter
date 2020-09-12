fe55a01eedb29ce19461644a33a7d484864d7c32
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-12 12:56:54 +0530

    data locale

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsRoutingModule } from './forms-routing.module';
 import { TodoComponent } from './todo/todo.component';
 import { SharedModule } from '../shared/shared.module';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [TodoComponent],
@@ -14,5 +15,6 @@ import { SharedModule } from '../shared/shared.module';
     ReactiveFormsModule,
     SharedModule,
   ],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'en-IN' }],
 })
 export class FormsModule {}
