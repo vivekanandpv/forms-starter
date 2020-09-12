75d550770759eab832847f0bf14d5e145faaa2e8
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-12 11:35:31 +0530

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PlaygroundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
