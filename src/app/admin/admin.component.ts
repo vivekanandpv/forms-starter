e517adb06bb9415280718ef05df0d206cc6354c8
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-13 14:46:12 +0530

    customized data table

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,6 +9,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { RestService } from '../rest.service';
+import { mockData } from './data.js';
 
 @Component({
   selector: 'app-admin',
@@ -16,59 +17,20 @@ import { RestService } from '../rest.service';
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit, AfterViewInit {
-  /** Constants used to fill up our data base. */
-  COLORS: string[] = [
-    'maroon',
-    'red',
-    'orange',
-    'yellow',
-    'olive',
-    'green',
-    'purple',
-    'fuchsia',
-    'lime',
-    'teal',
-    'aqua',
-    'blue',
-    'navy',
-    'black',
-    'gray',
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'gender',
   ];
-  NAMES: string[] = [
-    'Maia',
-    'Asher',
-    'Olivia',
-    'Atticus',
-    'Amelia',
-    'Jack',
-    'Charlotte',
-    'Theodore',
-    'Isla',
-    'Oliver',
-    'Isabella',
-    'Jasper',
-    'Cora',
-    'Levi',
-    'Violet',
-    'Arthur',
-    'Mia',
-    'Thomas',
-    'Elizabeth',
-  ];
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  dataSource: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private restService: RestService) {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) =>
-      this.createNewUser(k + 1)
-    );
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.dataSource = new MatTableDataSource(mockData);
   }
 
   ngOnInit(): void {}
@@ -93,29 +55,4 @@ export class AdminComponent implements OnInit, AfterViewInit {
       alert(response.message);
     });
   }
-
-  /** Builds and returns a new User. */
-  createNewUser(id: number): UserData {
-    const name =
-      this.NAMES[Math.round(Math.random() * (this.NAMES.length - 1))] +
-      ' ' +
-      this.NAMES[Math.round(Math.random() * (this.NAMES.length - 1))].charAt(
-        0
-      ) +
-      '.';
-
-    return {
-      id: id.toString(),
-      name: name,
-      progress: Math.round(Math.random() * 100).toString(),
-      color: this.COLORS[Math.round(Math.random() * (this.COLORS.length - 1))],
-    };
-  }
-}
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
 }
